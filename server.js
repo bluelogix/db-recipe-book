c0bf3adfe3a08b0a6c6aa1c6906e986ad53d4d8b
Lambda_School_Loaner_40 <lambda_school_loaner_40@Lambda-School-Loaner-40s-MacBook-Pro.local>
2019-02-28 18:54:28 -0500

    added migrations

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const helmet = require('helmet');
 
-// const recipeRouter = require('./routers/cohort-router.js');
+// const recipeRouter = require('./routers/recipe-router.js');
 
 const server = express();
 
 server.use(helmet());
 server.use(express.json());
 
-// server.use('/api/cohorts', recipeRouter);
+// server.use('/api/recipes', recipeRouter);
 
 module.exports = server;
